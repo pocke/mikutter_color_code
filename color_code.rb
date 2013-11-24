738ed4bfc3d53ccf4a18b9406ef740a509db2855
pocke <p.ck.t22@gmail.com>
2013-11-24 20:35:14 +0900

    refactor

--- color_code.rb
+++ color_code.rb
@@ -5,21 +5,14 @@
 # http://opensource.org/licenses/mit-license.php
 
 Plugin.create(:color_code) do
-
-  filter_message_background_color do | mp, color |
+  filter_message_background_color do |mp, color|
     if !mp.to_message.system? then
-      hashtags = mp.to_message[:entities][:hashtags]
-      hashtags.each do | tag |
-        val = tag[:text]
-        if /^[0-9a-fA-F]{6,6}$/ =~ val then
-          color = Array.new(3)
-          3.times do |i|
-            color[i] = val[i*2,2].to_i(16) << 8
-          end
-        end
+      tags = mp.to_message[:entities][:hashtags]
+      tags.each do |tag|
+        text = tag[:text]
+        color = text.scan(/.{2}/).map{|t| t.hex << 8} if /^[0-9a-fA-F]{6,6}$/ =~ text
       end
     end
     [mp, color]
   end
-
 end
