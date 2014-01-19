a71bd807240233f0de8f5d710b18d55c3bf9e860
pocke <p.ck.t22@gmail.com>
2014-01-19 17:44:28 +0900

    refactor

--- color_code.rb
+++ color_code.rb
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2013, pocket
+# Copyright 2013-2014, pocke
 # Licensed MIT
 # http://opensource.org/licenses/mit-license.php
 
 Plugin.create(:color_code) do
   filter_message_background_color do |mp, color|
-    if !mp.to_message.system? then
-      tags = mp.to_message[:entities][:hashtags]
-      tags.each do |tag|
+    unless mp.to_message.system? then
+      mp.to_message[:entities][:hashtags].each do |tag|
         text = tag[:text]
-        color = text.scan(/.{2}/).map{|t| t.hex << 8} if /^[0-9a-fA-F]{6,6}$/ =~ text
+        if /^[0-9a-fA-F]{6}$/ =~ text then
+          color = text.scan(/.{2}/).map{|t| t.hex << 8}
+        end
       end
     end
     [mp, color]
