4b463b80c484fee949df8b49e196f92873132531
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-09-30 23:49:57 +0900

    Fix deprecated warning

--- color_code.rb
+++ color_code.rb
@@ -6,8 +6,8 @@
 
 Plugin.create(:color_code) do
   filter_message_background_color do |mp, color|
-    unless mp.to_message.system? then
-      mp.to_message[:entities][:hashtags].each do |tag|
+    unless mp.message.system? then
+      mp.message[:entities][:hashtags].each do |tag|
         text = tag[:text]
         if /^[0-9a-fA-F]{6}$/ =~ text then
           color = text.scan(/.{2}/).map{|t| t.hex << 8}
